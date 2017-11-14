f29cd1342a2e08fbd483712dea638a2a93e26da4
Simon Alling <alling.simon@gmail.com>
2017-11-14 17:37:02 +0100

    Add type annotations in utilities.py

--- utilities.py
+++ utilities.py
@@ -1,12 +1,17 @@
-def first(tuple):
+from typing import Any, TypeVar, Tuple
+
+A = TypeVar('A')
+B = TypeVar('B')
+
+def first(tuple: Tuple[A, B]) -> A:
     (a, b) = tuple
     return a
 
 
-def second(tuple):
+def second(tuple: Tuple[A, B]) -> B:
     (a, b) = tuple
     return b
 
 
-def isSomething(x):
+def isSomething(x: Any) -> bool:
     return x != None
