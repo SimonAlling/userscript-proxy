5c1f35d45efa1feb6be547b4045c2db16e72623e
Simon Alling <alling.simon@gmail.com>
2019-07-06 01:37:54 +0200

    Improve ignore/intercept mechanism
    This commit makes the mechanism for specifying ignore/intercept rules
    more flexible: The user can now specify a glob pattern using the
    --ignore or --intercept flag. This is especially useful in light of the
    fact that hostname-based rules may not work in transparent mode, so one
    might want to have different rules for regular and transparent mode.
    
    https://docs.mitmproxy.org/stable/howto-ignoredomains/#limitations

--- modules/text.py
+++ modules/text.py
@@ -1,5 +1,8 @@
 from modules.constants import DEFAULT_PORT
 
+metavar_file = "FILE"
+matching = f"matching any of the rules specified in {metavar_file} (file name or glob pattern)"
+
 description = "Inject userscripts using mitmproxy."
 option_inline = "inline"
 help_inline = "Always insert userscripts inline, never linked"
@@ -7,7 +10,9 @@ option_verbose = "verbose"
 help_verbose = "Insert an HTML comment with a list of injected userscripts"
 option_transparent = "transparent"
 help_transparent = "Transparent mode"
-option_whitelist = "whitelist"
-help_whitelist = "Whitelist mode"
+option_intercept = "intercept"
+help_intercept = "Intercept only traffic from hosts " + matching
+option_ignore = "ignore"
+help_ignore = "Intercept all traffic except from hosts " + matching
 option_port = "port"
 help_port = f"""mitmproxy port (default: {DEFAULT_PORT})"""
