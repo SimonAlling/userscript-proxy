b35c4eeb66532ca104f9c1093efca6670657cf45
Simon Alling <alling.simon@gmail.com>
2019-07-06 11:15:32 +0200

    Sort arguments alphabetically

--- modules/text.py
+++ modules/text.py
@@ -4,15 +4,21 @@ metavar_file = "FILE"
 matching = f"matching any of the rules specified in {metavar_file} (file name or glob pattern)"
 
 description = "Inject userscripts using mitmproxy."
-option_inline = "inline"
-help_inline = "Always insert userscripts inline, never linked"
-option_verbose = "verbose"
-help_verbose = "Insert an HTML comment with a list of injected userscripts"
-option_transparent = "transparent"
-help_transparent = "Transparent mode"
-option_intercept = "intercept"
-help_intercept = "Intercept only traffic from hosts " + matching
+
 option_ignore = "ignore"
 help_ignore = "Intercept all traffic except from hosts " + matching
+
+option_intercept = "intercept"
+help_intercept = "Intercept only traffic from hosts " + matching
+
+option_inline = "inline"
+help_inline = "Always insert userscripts inline, never linked"
+
 option_port = "port"
 help_port = f"""mitmproxy port (default: {DEFAULT_PORT})"""
+
+option_transparent = "transparent"
+help_transparent = "Transparent mode"
+
+option_verbose = "verbose"
+help_verbose = "Insert an HTML comment with a list of injected userscripts"
