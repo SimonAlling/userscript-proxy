0f7dad779b60722336aed9fbd721166ebb82be5a
Simon Alling <alling.simon@gmail.com>
2019-07-13 02:23:33 +0200

    v0.11.0

--- modules/constants.py
+++ modules/constants.py
@@ -1,6 +1,6 @@
 VERSION_PREFIX: str = "v"
 APP_NAME: str = "Userscript Proxy"
-VERSION: str = "0.10.0"
+VERSION: str = "0.11.0"
 ATTRIBUTE_UP_VERSION: str = "data-userscript-proxy-version"
 DEFAULT_PORT: int = 8080
 DEFAULT_USERSCRIPTS_DIR: str = "userscripts"
