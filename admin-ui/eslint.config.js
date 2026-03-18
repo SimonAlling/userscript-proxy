7361ca7857b45bd9111f977f7f6d6752c6d2a486
Simon Alling <alling.simon@gmail.com>
2026-03-18 16:50:41 +0100

    Add backend workspace

--- admin-ui/eslint.config.js
+++ admin-ui/eslint.config.js
@@ -19,6 +19,7 @@ export default defineConfig([
       parserOptions: {
         project: [
           "./core/tsconfig.json",
+          "./backend/tsconfig.json",
           "./frontend/tsconfig.node.json",
           "./frontend/tsconfig.app.json",
         ],
