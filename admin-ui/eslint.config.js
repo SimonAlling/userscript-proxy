67f735db33c83122044860305b1e3c104d0a34cc
Simon Alling <alling.simon@gmail.com>
2026-03-18 16:28:58 +0100

    Remove unused globals import

--- admin-ui/eslint.config.js
+++ admin-ui/eslint.config.js
@@ -1,5 +1,4 @@
 import js from "@eslint/js";
-import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
