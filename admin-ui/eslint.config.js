8e24a08d0c4c9f80b76355095e6394bba27b335f
Simon Alling <alling.simon@gmail.com>
2026-03-23 15:37:34 +0100

    Add semi-automated formatting check for admin UI
    The new `tsconfig.json` is necessary for `npm run lint` to succeed.
    
    COPYing `.gitignore` is necessary for Prettier to ignore the `dist/` directories when building with Docker (e.g. `docker compose up --build`).

--- admin-ui/eslint.config.js
+++ admin-ui/eslint.config.js
@@ -18,6 +18,7 @@ export default defineConfig([
     languageOptions: {
       parserOptions: {
         project: [
+          "./tsconfig.json",
           "./core/tsconfig.json",
           "./backend/tsconfig.json",
           "./frontend/tsconfig.node.json",
---
+++ admin-ui/prettier.config.ts
@@ -0,0 +1,8 @@
+export default {
+  overrides: [
+    {
+      files: ["tsconfig*.json"],
+      options: { parser: "jsonc" },
+    },
+  ],
+};
